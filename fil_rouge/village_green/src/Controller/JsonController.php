3799d7bd2c5b6db482e18ad43802b290f41f53e3
harold <s.lefebvre907@laposte.net>
2022-03-10 11:48:30 +0100

    mise à jour

--- fil_rouge/village_green/src/Controller/JsonController.php
+++ fil_rouge/village_green/src/Controller/JsonController.php
@@ -16,56 +16,36 @@ class JsonController extends AbstractController
     /**
      * @Route("/json/produits", name="json")
      */
-    public function produits(ProductsRepository $repo,SerializerInterface  $serializer): Response
+    public function produits(ProductsRepository $repo): Response
     {
 
         $produit = $repo->findAll();
 
 
-        $response = $serializer->serialize(
-            $produit,
-            'json',
-            ['groups' => 'show_product']
-        );
-
-        return new JsonResponse($response, 200, [],true);
-
+        return $this->json($produit,200,[],['groups' => 'show_product']);
     }
 
     /**
      * @Route("/json/categorie", name="categorie")
      */
-    public function categorie(CategoriesRepository $repo,SerializerInterface  $serializer): Response
+    public function categorie(CategoriesRepository $repo): Response
     {
 
         $categorie = $repo->findAll();
 
-
-        $response = $serializer->serialize(
-            $categorie,
-            'json',
-            ['groups' => 'showcat']
-        );
-
-        return new JsonResponse($response, 200, [],true);
-
+        return $this->json($categorie,200,[],['groups' => 'showcat']);
     }
     /**
      * @Route("/json/employee", name="employee")
      */
-    public function employee(EmployeesRepository $repo,SerializerInterface  $serializer): Response
+    public function employee(EmployeesRepository $repo): Response
     {
 
         $employee = $repo->findAll();
 
 
-        $response = $serializer->serialize(
-            $employee,
-            'json',
-            ['groups' => 'showemp']
-        );
+        return $this->json($employee,200,[],['groups' => 'showemp']);
 
-        return new JsonResponse($response, 200, [],true);
 
     }
 }
