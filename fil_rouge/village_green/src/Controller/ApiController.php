d2cb23a78c3d2fadd7b516504ad85250c392c21c
harold <s.lefebvre907@laposte.net>
2022-03-11 09:22:45 +0100

    mise à jour

--- fil_rouge/village_green/src/Controller/ApiController.php
+++ fil_rouge/village_green/src/Controller/ApiController.php
@@ -6,13 +6,16 @@ use App\Entity\Products;
 use App\Repository\CategoriesRepository;
 use App\Repository\EmployeesRepository;
 use App\Repository\ProductsRepository;
+use phpDocumentor\Reflection\DocBlock\Serializer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Exception\NotEncodableValueException;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiController extends AbstractController
 {
-
 
 
     /**
@@ -23,18 +26,40 @@ class ApiController extends AbstractController
 
         return $this->json($repo->findAll(), 200, [], ['groups' => 'show_product']);
     }
+
     /**
      * @Route("/api/produits/{id}", name="json_produits",  methods={"get"})
      */
-    public function produit(Products $id): Response
+    public function Produit(Products $id): Response
     {
 
         return $this->json($id, 200, [], ['groups' => 'show_product']);
     }
+
+    /**
+     * @Route("/api/produits/{id}", name="put_produits",  methods={"put"})
+     */
+    public function PutProduit(Products $id, Request $request, SerializerInterface $serializer): \Symfony\Component\HttpFoundation\JsonResponse
+    {
+        try {
+
+            $post = $serializer->deserialize($request->getContent(), Products::class, 'json');
+            return $this->json($post, 201, [], ['groups' => "show_products"]);
+        } catch (NotEncodableValueException $e) {
+            return $this->json([
+                'status' => 400,
+            'message'=>$e->getMessage()
+                ],400
+            );
+        }
+
+//        re// dd($post);
+    }
+
     /**
      * @Route("/api/categorie", name="categorie", methods={"get"})
      */
-    public function categorie(CategoriesRepository $repo): Response
+    public function Categorie(CategoriesRepository $repo): Response
     {
         return $this->json($repo->findAll(), 200, [], ['groups' => 'showcat']);
     }
@@ -42,7 +67,7 @@ class ApiController extends AbstractController
     /**
      * @Route("/api/employee", name="employee", methods={"get"})
      */
-    public function employee(EmployeesRepository $repo): Response
+    public function Employee(EmployeesRepository $repo): Response
     {
 
         return $this->json($repo->findAll(), 200, [], ['groups' => 'showemp']);
