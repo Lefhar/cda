ce6bb45a3483726bf756c7a0843a3c1290135869
harold <s.lefebvre907@laposte.net>
2022-05-15 18:03:59 +0200

    ajout fonctionnalité count panier avec ajax

--- fil_rouge/village_green/public/assets/js/script.js
+++ fil_rouge/village_green/public/assets/js/script.js
@@ -2,14 +2,28 @@ $(document).ready(function () {
     $('[data-toggle="popover"]').popover({trigger: "hover", container: 'body'});
 
     //console.log(panier)
-function updatePanier(count){
-    let panier = document.getElementsByClassName('badge badge-pill badge-danger');
-    for (let i = 0; i < panier.length; i += 1) {
+    function updateCountPanier(count) {
+        let panier = document.getElementsByClassName('badge badge-pill badge-danger');
+        for (let i = 0; i < panier.length; i += 1) {
 
-        console.log(parseInt(panier[i].innerHTML))
-        panier[i].innerHTML = count;
+            // console.log(parseInt(panier[i].innerHTML))
+            panier[i].innerHTML = count;
+        }
     }
-}
+
+    function updatePanier(newData) {
+
+
+        let objpanier = document.getElementsByClassName('panier');
+
+        for (let i = 0; i < objpanier.length; i += 1) {
+            let dataVal = objpanier[i].getAttribute("data-content");
+            objpanier[i].setAttribute("data-content", newData);
+            // console.log(newData)
+            // panier[i].innerHTML = count;
+        }
+    }
+
     $("form").submit(function (event) {
         event.preventDefault();
         // console.log(event.target.action)
@@ -19,27 +33,25 @@ function updatePanier(count){
             type: "POST",
             dataType: 'json',
             data: formData
-        }).done(function (response) { //
-            //     console.log(formData);
-            //  $(this).form().getElementsByTagName('qte').val() = formData.qte;
-
-            // const obj = response;
-            // //
-            //  let resultat = 0;
-            // //
-            // for (let i = 0; i < obj.length; i += 1) {
-            //
-            //     resultat +=  parseInt(obj[cart].qte);
-            //     console.log('teste addition '+resultat)
-            //
-            // }
+        }).done(function (response) {
             let resultat = 0;
-            $.each(response, function(key, val) {
-                console.log(val.qte);
-                 resultat += parseInt(val.qte);
+            let prix = 0;
+            let temp = "";
+            $.each(response, function (key, val) {
+                // console.log(val.qte);
+                resultat += parseInt(val.qte);
+                prix += parseInt(val.prix) * parseInt(val.qte);
+                let teste = ` <p><img height='50px' src='/assets/src/${val.photo}' alt='${val.name}'> ${val.name}</p>
+<p>Quantité : <span class='text-success'>${val.qte}</span>  Prix : <span class='text-success'>${val.prix}€</span></p><hr>
+`
+                temp += teste;
             });
-            console.log(response);
-            updatePanier(resultat)
+            let total = `<p>Total : <span class='text-success'>  ${prix} €</span> </p>`;
+           temp = temp + total;
+            // console.log(response);
+            // console.log(prix);
+            updateCountPanier(resultat)
+            updatePanier(temp)
 
 
             //  console.log('teste addition '+obj)
